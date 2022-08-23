ae517c432e7e938008c2bbe751571ce55cf6d2aa
Ashini Sagaran <20087371@mail.wit.ie>
2022-08-23 15:55:27 +0100

    Mongo lab Users Router

--- movie_api/index.js
+++ movie_api/index.js
@@ -1,9 +1,10 @@
 import './db';
 import dotenv from 'dotenv';
 import express from 'express';
-// import moviesRouter from './api/movies';
+import moviesRouter from './api/movies';
 import genresRouter from './api/genre';
-import './seedData'
+import './seedData';
+import usersRouter from './api/users';
 
 dotenv.config();
 
@@ -13,8 +14,14 @@ const port = process.env.PORT;
 
 app.use(express.json());
 
-// app.use('/api/movies', moviesRouter);
+//Movies router
+app.use('/api/movies', moviesRouter);
+
+//Genres router
 app.use('/api/genres', genresRouter);
+
+//Users router
+app.use('/api/users', usersRouter);
 
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
